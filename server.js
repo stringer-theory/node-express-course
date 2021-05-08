bf598014c12538e1a7c647478e64f2d42e5f91f9
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-05-08 06:16:00 -0500

    add custom function to lookup user by num

--- server.js
+++ server.js
@@ -46,6 +46,17 @@ app.post('/login',function(req,res){
     }
 })
 
+app.get('/getUserByNum/:num',function(req,res){
+    console.log(req.params.num)
+    console.log(mockUserData[req.params.num])
+    res.json({
+        success: true,
+        message: 'looked up one user',
+        user: mockUserData[req.params.num]
+    })
+})
+
+
 app.listen(8000,function(){
     console.log("server is running")
     })
